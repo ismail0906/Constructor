3d651447448f2f416df65e809839d73c0942c1c8
ASUS <ASUS@ISMAIL>
2022-10-30 11:24:08 +0300

    Add project files.

---
+++ Ogrenci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Constructor
+{
+    public class Ogrenci
+    {
+        public Ogrenci(string isim)
+        {
+            Adi = isim;
+
+        }
+        public Ogrenci()
+        {
+
+        }
+
+        public string Adi { get; set; }
+        public string SoyAdi { get; set; }
+
+
+
+        ~Ogrenci() 
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _3_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci("aaa");
+            
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.Adi = "bbb";
+
+            Ogrenci ogr2 = new Ogrenci();
+            ogr2.Adi = "ccc";
+
+            Console.WriteLine(ogr2.Adi);
+
+            Console.ReadKey();
+        }
+    }
+}
